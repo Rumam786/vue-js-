f45bfe047cdc5f33e188c80e565590342db54a39
Ruman SUkhera <92282309+Rumam786@users.noreply.github.com>
2021-11-18 22:55:19 +0500

    Add files via upload

--- signup.vue
+++ signup.vue
@@ -1,31 +1,43 @@
 <template>
-    <div>
-        <input type="text" placeholder="enter your name" /><br><br>
-        <input type="text" placeholder="enter password" /><br><br>
-        <p>Choose Hoobies</p>
-        <input type="checkbox" id="cricket" value="cricket"/>
-        <label for="cricket"> Cricket
-            </label>
-             <input type="checkbox" id="football" value="football"/>
-        <label for="football"> Football
-            </label>
-            <br><br>
-            <p>Choose Gender</p>
-             <input type="radio" id="male" value="male"/>
-        <label for="male"> Male
-            </label>
-             <input type="radio" id="female" value="female"/>
-        <label for="female"> Female
-            </label>
-    </div>
+<div>
+    <input type="text" placeholder="enter your name" v-model="signup.username" /><br><br>
+    <input type="text" placeholder="enter password" v-model="signup.password" /><br><br>
+    <p>Choose Hoobies</p>
+    <input type="checkbox" id="cricket" value="cricket" v-model="signup.hoobies" />
+    <label for="cricket"> Cricket
+    </label>
+    <input type="checkbox" id="football" value="football" v-model="signup.hoobies" />
+    <label for="football"> Football
+    </label>
+    <br><br>
+    <p>Choose Gender</p>
+    <input type="radio" id="male" value="male" v-model="signup.gender" />
+    <label for="male"> Male
+    </label>
+    <input type="radio" id="female" value="female" v-model="signup.gender" />
+    <label for="female"> Female
+    </label><br><br>
+    <button v-on:click="signupUser">signup</button>
+</div>
 </template>
 
 <script>
-    export default {
-        name:'signup'
-    }
+export default {
+    name: 'signup',
+    data() {
+        return {
+            signup: {
+                username: null,
+                password: null,
+                hoobies: [],
+                gender: null
+            }
+        }
+    },
+    methods: {
+        signupUser() {
+            console.warn(this.signup)
+        }
+    },
+}
 </script>
-
-<style lang="scss" scoped>
-
-</style>
