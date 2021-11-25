bbf45ef58e4e10bb7188cdb2719dc3f5faa1bb58
Ruman SUkhera <92282309+Rumam786@users.noreply.github.com>
2021-11-25 07:27:49 +0500

    Add files via upload

---
+++ conditions.vue
@@ -0,0 +1,28 @@
+<template>
+    <div>
+        <h1 v-if="show">conditional rendering if condition</h1>
+        <h1 v-else>conditional rendering else block</h1>
+        <button v-on:click="display">toogle element</button>
+
+    </div>
+</template>
+
+<script>
+    export default {
+        name:'conditions',
+        data() {
+            return {
+                show: true
+            }
+        },
+        methods: {
+            display() {
+                this.show=!this.show
+            }
+        },
+    }
+</script>
+
+<style lang="scss" scoped>
+
+</style>
